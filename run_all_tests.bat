08118bde735c40c34b25ca2c4ff27905b7dae4d6
Vladimir <vladimir.shylov@outlook.com>
2026-07-08 22:14:39 +0300

    update: modify cargo test command in run_all_tests.bat to use single thread
    Adjusted the cargo test command in the run_all_tests.bat script to run tests using a single thread. This change aims to improve test execution consistency in a Windows environment.

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -4,4 +4,4 @@ chcp 65001 > nul
 set MINDFORK_ENGINE_URL=http://192.168.1.20:8000/v1
 set MINDFORK_EMBED_URL=http://192.168.1.20:8001/v1
 
-cargo test -- --include-ignored
+cargo test -- --include-ignored --test-threads=1
